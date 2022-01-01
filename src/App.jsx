93a46b7bd8e6391ae46fe8d4401a0cdf30261b27
Miguel-Leite <miguelleite200leite@gmail.com>
2022-01-01 17:41:14 +0100

    styled box course

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import './App.css';
+import 'font-awesome/css/font-awesome.css';
 import svgHeader from './svg-header.svg'
 import { Menu } from './components/Navbars';
 import { NavLink } from 'react-router-dom';
+import { CourseItem } from './components/CourseItem';
 
 function App() {
   return (
@@ -26,20 +28,24 @@ function App() {
         </div>
       </header>
 
-      <main className='__main'>
-        <div className='__main-content'>
-          <h3>100's of Courses</h3>
-          <h6>Class aptent taciti sociosqui ad litora torquint por comubia nostra</h6>
-          <p>
-            in ea fugit consectetur reiciendis dolor sit, amet 
-            consectetur adipisicing elit. Excepturi commodi omnis 
-            in ea fugit consectetur reiciendis hic nam sapiente, 
-            numquam consequatur libero qui obcaecati ipsa. Eligendi 
-            blanditiis sapiente pariatur? Earum!
-          </p>
-        </div>
-        
-      </main>
+      <div className='container'>
+        <main className='__main'>
+            <div className='__main-content'>
+              <h3>100's of Courses</h3>
+              <h6>Class aptent taciti sociosqui ad litora torquint por comubia nostra</h6>
+              <p>
+                in ea fugit consectetur reiciendis dolor sit, amet 
+                consectetur adipisicing elit. Excepturi commodi omnis 
+                in ea fugit consectetur reiciendis hic nam sapiente, 
+                numquam consequatur libero qui obcaecati ipsa. Eligendi 
+                blanditiis sapiente pariatur? Earum!
+              </p>
+            </div>
+            <div className='__courses'>
+              <CourseItem />
+            </div>
+        </main>
+      </div>
     </div>
   )
 }
