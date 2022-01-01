45025cb13890a3bd4147aafbc1da1dd101b37d1f
Miguel-Leite <miguelleite200leite@gmail.com>
2022-01-01 20:01:22 +0100

    create area easy

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import 'font-awesome/css/font-awesome.css';
 import svgHeader from './svg-header.svg'
+import Book from './Book.svg'
 import { Menu } from './components/Navbars';
 import { NavLink } from 'react-router-dom';
 import { CourseItem } from './components/CourseItem';
@@ -28,7 +29,7 @@ function App() {
         </div>
       </header>
 
-      <div className='container'>
+      <div className='__container'>
         <main className='__main'>
             <div className='__main-content'>
               <h3>100's of Courses</h3>
@@ -39,13 +40,35 @@ function App() {
                 in ea fugit consectetur reiciendis hic nam sapiente, 
                 numquam consequatur libero qui obcaecati ipsa. Eligendi 
                 blanditiis sapiente pariatur? Earum!
-              </p>
+              </p> <br /> <br /> 
+
+              <NavLink to='/' className='__btn __btn-course'> View All Courses </NavLink>
             </div>
             <div className='__courses'>
               <CourseItem />
             </div>
         </main>
       </div>
+      <section className='__easy'>
+          <div className='__container'>
+            <div className='__easy-inner'>
+              <div className='__image'>
+                <img src={Book} />
+              </div>
+              <div className='__content'>
+                <h2> Easy As 1...2...3... </h2>
+                <p>
+                  in ea fugit consectetur reiciendis dolor sit, amet 
+                  consectetur adipisicing elit. Excepturi commodi omnis 
+                  in ea fugit consectetur reiciendis hic nam sapiente, 
+                  numquam consequatur libero qui obcaecati ipsa. Eligendi 
+                  blanditiis sapiente pariatur? Earum!
+                </p> <br />
+                <NavLink to='/' className='__btn __btn-easy'> START A FREE TRIAL <i className="fa fa-arrow-right" aria-hidden="true"></i> </NavLink>
+              </div>
+            </div>
+          </div>
+        </section>
     </div>
   )
 }
